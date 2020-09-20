0e5e651b01fc1b3106cbd20bd9308946c2242ebc
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2020-09-20 15:27:21 -0300

    Add project files.

---
+++ openssl-sspi/openssl-sspi.cpp
@@ -0,0 +1,12 @@
+﻿// openssl-sspi.cpp : Defines the entry point for the application.
+//
+
+#include "openssl-sspi.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello CMake." << endl;
+    return 0;
+}
