c50020bdd532a32513f2d017a9aaf22d49fbcedf
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2020-11-21 03:28:00 -0300

    Add Windows support to httpclient_nettest
    Use winsock headers on WIN32 instead of BSD sockets, link to Winsock
    libraries.
    
    I think it still won't *run* because I'm missing WSAStartup.

--- httpclient_nettest.cpp
+++ httpclient_nettest.cpp
@@ -2,11 +2,20 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+#ifdef WIN32
+# define _WIN32_WINNT 0x0600
+#endif
+
 #include "httpclient.h"
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#ifdef WIN32
+# include <winsock2.h>
+# include <ws2tcpip.h>
+#else
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+#endif
 
 #include <cassert>
 #include <cstdio>
