38036d5e33375c12ecb3e280f481c6017dbea049
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2020-11-28 21:41:50 -0300

    Implement InitializeSecurityContext stub
    Like AcquireCredentialsHandle, it creates an object and returns a handle
    to it, but doesn't have any other functionality yet.
    
    It looks like curl isn't calling this yet. I suspect it's calling
    QueryCredentialsAttributes first and that's still missing.

--- ssp.cpp
+++ ssp.cpp
@@ -76,6 +76,33 @@ SSPCredentials::SSPCredentials() {
     ;
 }
 
+class SSPContext {
+public:
+    static const UINT32 MAGIC = 0xbe80c313;
+
+    SSPContext();
+    CtxtHandle toHandle() const;
+    static SSPContext* fromHandle(PCtxtHandle handle);
+
+private:
+    // non-copyable
+    SSPContext(const SSPContext&) = delete;
+    SSPContext& operator=(const SSPContext&) = delete;
+};
+CtxtHandle SSPContext::toHandle() const {
+    return { MAGIC, reinterpret_cast<UINT_PTR>(this) };
+}
+SSPContext* SSPContext::fromHandle(PCtxtHandle handle) {
+    if (handle->dwLower == MAGIC) {
+        return reinterpret_cast<SSPContext*>(handle->dwUpper);
+    } else {
+        return nullptr;
+    }
+}
+SSPContext::SSPContext() {
+    ;
+}
+
 extern "C"
 SECURITY_STATUS SEC_ENTRY myAcquireCredentialsHandleW(
     _In_opt_  SEC_WCHAR*     pszPrincipal,
@@ -99,7 +126,7 @@ extern "C"
 SECURITY_STATUS SEC_ENTRY myFreeCredentialsHandle(PCredHandle phCredential)
 {
     printf("[testssp] FreeCredentialsHandle(%p)\n", phCredential);
-    SSPCredentials* cred = SSPCredentials::fromHandle(phCredential);
+    auto* cred = SSPCredentials::fromHandle(phCredential);
     if (cred) {
         delete cred;
         return SEC_E_OK;
@@ -108,6 +135,47 @@ SECURITY_STATUS SEC_ENTRY myFreeCredentialsHandle(PCredHandle phCredential)
     }
 }
 
+extern "C"
+SECURITY_STATUS SEC_ENTRY myInitializeSecurityContextW(
+    _In_opt_    PCredHandle    phCredential,
+    _In_opt_    PCtxtHandle    phContext,
+    _In_opt_    SEC_WCHAR*     pszTargetName,
+    _In_        unsigned long  fContextReq,
+    _In_        unsigned long  Reserved1,
+    _In_        unsigned long  TargetDataRep,
+    _In_opt_    PSecBufferDesc pInput,
+    _In_        unsigned long  Reserved2,
+    _Inout_opt_ PCtxtHandle    phNewContext,
+    _Inout_opt_ PSecBufferDesc pOutput,
+    _Out_       unsigned long* pfContextAttr,
+    _Out_opt_   PTimeStamp     ptsExpiry
+) {
+    printf("[testssp] InitializeSecurityContext credential '%p' (%p) pcontext '%p' target name '%ls' contextReq 0x%ux targetdatarep %u pInput (%d buffers), pnewcontext %p, pOutput (%d buffers), pContextAttr %p pexpiry %p\n",
+        phCredential, phCredential ? phCredential->dwUpper : 0, phContext, pszTargetName, fContextReq, TargetDataRep, pInput->cBuffers, phNewContext, pOutput->cBuffers, pfContextAttr, ptsExpiry);
+    if (!phContext) {
+        SSPContext* ctx = new SSPContext();
+        *phNewContext = ctx->toHandle();
+        return SEC_E_OK;
+    }
+    return SEC_E_INTERNAL_ERROR;
+}
+
+extern "C"
+SECURITY_STATUS SEC_ENTRY myDeleteSecurityContext(
+    PCtxtHandle phContext
+) {
+    printf("[testssp] DeleteSecurityContext(%p)\n", phContext);
+    auto* ctx = SSPContext::fromHandle(phContext);
+    if (ctx) {
+        delete ctx;
+        return SEC_E_OK;
+    }
+    else {
+        return SEC_E_INVALID_HANDLE;
+    }
+}
+
+
 SecurityFunctionTableW g_functionTable = {
     SECURITY_SUPPORT_PROVIDER_INTERFACE_VERSION, // dwVersion
     &myEnumerateSecurityPackagesW, // EnumerateSecurityPackagesW
@@ -115,10 +183,10 @@ SecurityFunctionTableW g_functionTable = {
     &myAcquireCredentialsHandleW, // AcquireCredentialsHandleW
     &myFreeCredentialsHandle, // FreeCredentialsHandle
     nullptr, // Reserved2
-    nullptr, // InitializeSecurityContextW
+    &myInitializeSecurityContextW, // InitializeSecurityContextW
     nullptr, // AcceptSecurityContext
     nullptr, // CompleteAuthToken
-    nullptr, // DeleteSecurityContext
+    &myDeleteSecurityContext, // DeleteSecurityContext
     nullptr, // ApplyControlToken
     nullptr, // QueryContextAttributesW
     nullptr, // ImpersonateSecurityContext
